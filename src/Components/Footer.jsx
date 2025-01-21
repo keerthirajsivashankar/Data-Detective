51caddc717b6c865f8ae2cfac04cff0b69c919e8
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-01-21 21:54:37 +0530

    New Design

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,48 @@
 import React from "react";
-
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>&copy; 2025 Fraud Detection App. All rights reserved.</p>
+        <div className="content">
+            <div className="main">
+            <div className="col">
+                <h4>Links</h4>
+                <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h4>Our Services</h4>
+                <ul>
+                <li><a href="#">Web Development</a></li>
+                <li><a href="#">Web Designing</a></li>
+                <li><a href="#">Product Management</a></li>
+                <li><a href="#">Marketing</a></li>
+                <li><a href="#">Graphics</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h4>Products</h4>
+                <ul>
+                <li><a href="#">Fraud Detection</a></li>
+                <li><a href="#">Data Cleaning</a></li>
+                <li><a href="#">Anamoly Detection</a></li>
+                <li><a href="#">CSV File conversion</a></li>
+                </ul>
+            </div>
+            <div className="col">
+                <h4>Follow Us</h4>
+                <ul>
+                <li><a href="#">Fraud Detection</a></li>
+                <li><a href="#">Data Cleaning</a></li>
+                <li><a href="#">Anamoly Detection</a></li>
+                <li><a href="#">CSV File conversion</a></li>
+                </ul>
+            </div>
+            </div>
+        </div>
     </footer>
   );
 };
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import './Header.css'
 const Header = () => {
   return (
     <header className="header">
-      <h1>Fraud Detection App</h1>
       <nav>
         <ul>
           <li><Link to="/">Home</Link></li>
