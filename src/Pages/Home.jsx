cbc1e8e192399826af18316156f63cb3aa7a4a65
keerthirajsivashankar <keerthirajsivashankar@gmail.com>
2025-01-24 13:32:20 +0530

    Gonna Complete Front Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css"
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleUploadClick = () => {
+    navigate("/processing"); // Navigate to the Processing page
+  };
   return (
     <div className="home-page">
       <div className="Image"><img src="" alt="" /></div>
       <div className="Upload">
-        <button className="ubtn"><h1>Upload File</h1></button>
+        <button className="ubtn" onClick={handleUploadClick}><h1>Upload File</h1></button>
         <br />
         <h3>Or drop a file</h3>
+        <br />   
       </div>
     </div>
   );
